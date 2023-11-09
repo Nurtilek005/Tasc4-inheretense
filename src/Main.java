835c19a227bafebcfbcc6d4b5b152e76c8f5682e
Nurtilek005 <140827106+Nurtilek005@users.noreply.github.com>
2023-11-09 13:18:24 +0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+                System.out.println(" ");
+                Programmer programmer = new Programmer("Luparic","aa","The Globe");
+                System.out.println(programmer);
+                programmer.learn();
+                programmer.walk();
+                programmer.eat();
+                programmer.coding();
+                System.out.println("==========================="+"\n");
+                Dancer dancer = new Dancer("Nurlan","bb","BTS");
+                System.out.println(dancer);
+                dancer.learn();
+                dancer.walk();
+                dancer.eat();
+                dancer.dancing();
+                System.out.println("==========================="+"\n");
+                Singer singer = new Singer("Nurtilek","cc","ll");
+                System.out.println(singer);
+                singer.learn();
+                singer.walk();
+                singer.eat();
+                singer.singing();
+                singer.playGuitar();
+            }
+        }
+
+
+
+
